0ead2178fcb1c7434c5048a538ea6a49bb55fae9
Tobias Bauer <TobiasBauer115@gmail.com>
2018-03-05 14:21:48 -0500

    Created FindTargetState

---
+++ Assets/Scripts/AI/States/FindTargetState.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindTargetState : BaseState
+{
+    private AIAgent _owner = null;
+
+    private float _elapsedTime = 0.0f;
+
+    public FindTargetState(AIAgent owner)
+    {
+        _stateDefinition.stateName = StateDefinition.StateName.FindTarget;
+        _owner = owner;
+    }
+
+    public override void OnEnter()
+    {
+        Debug.Log("Entering FindTargetState");
+        _elapsedTime = 0.0f;
+    }
+
+    public override void OnExit()
+    {
+        Debug.Log("Exiting FindTargetState");
+    }
+
+    public override void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+    }
+}
