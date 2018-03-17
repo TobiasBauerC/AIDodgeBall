0d8c2c49035aa63dd330704e562110ef3bbc7c88
Tobias Bauer <TobiasBauer115@gmail.com>
2018-03-17 08:44:23 -0400

    Added controls screen

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour 
 {
+    [SerializeField] private GameObject _panel;
+
     public void OnAIButton(int aiLevelIndex)
     {
         SceneManager.instance.GoToLevel(aiLevelIndex);
@@ -12,5 +15,10 @@ public class MainMenuManager : MonoBehaviour
     public void OnPhysicsButton(int physicsLevelIndex)
     {
         SceneManager.instance.GoToLevel(physicsLevelIndex);
+    }
+
+    public void OnControllsButton()
+    {
+        _panel.SetActive(!_panel.activeInHierarchy);
     }
 }
